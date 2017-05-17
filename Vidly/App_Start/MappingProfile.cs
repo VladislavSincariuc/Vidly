3d5cf018f04f90f528c73206e176b424db67b02a
vladislav.sincariuc <vladislav.sincariuc@gmail.com>
2017-05-17 17:56:35 +0200

    with ajax

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -13,10 +13,13 @@ namespace Vidly.App_Start
         public MappingProfile()
         {
             Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MembershipType, MembershipTypeDto>();
+
+
             Mapper.CreateMap<CustomerDto, Customer>()
                 .ForMember(c => c.Id, opt => opt.Ignore());
 
-            Mapper.CreateMap<Movie, MovieDto>();
             Mapper.CreateMap<MovieDto, Movie>()
                 .ForMember(c => c.Id, opt => opt.Ignore());
         }
--- Vidly/Dtos/CustomerDto.cs
+++ Vidly/Dtos/CustomerDto.cs
@@ -20,7 +20,9 @@ namespace Vidly.Dtos
 
         public byte MembershipTypeId { get; set; }
 
-        [Min18YearsIfAMember]
+        public MembershipTypeDto MembershipType { get; set; }
+
+        // [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
     }
 }
