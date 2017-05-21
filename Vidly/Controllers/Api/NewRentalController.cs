78c18a88ec6f459b1f5dc55daab9322a74871fcd
vladislav.sincariuc <vladislav.sincariuc@gmail.com>
2017-05-21 16:26:26 +0200

    add auto completation

--- Vidly/Controllers/Api/NewRentalController.cs
+++ Vidly/Controllers/Api/NewRentalController.cs
@@ -5,7 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using AutoMapper;
-using Glimpse.Mvc.AlternateType;
+using Glimpse.Mvc.AlternateType; 
 using Vidly.Dtos;
 using Vidly.Models;
 
